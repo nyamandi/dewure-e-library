fac05dd7569c76174cc5de9e72739c175a6729b3
nyamandi <nyamanditanaka6@gmail.com>
2024-08-08 11:03:38 +0200

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+    'DOMContentLoaded',function(){
+        const element =document.querySelector('body');
+body.style.backgroundImage = "url('C:\Users\aaa\Desktop\digital library\project resources\wallpaperflare.com_wallpaper.jpg')";
+body.style.backgroundSize= "cover";});
+
+
